4e1a02acae5e11c3a0272288c7eed475db9fcbfc
Júlio <juliopiubellow@gmail.com>
2018-01-05 13:53:03 -0200

    input email and password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import {Button, Card, CardSection} from './common';
+import {Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state={
+            email:'',
+            password:'',
+        }
+    }
+
     render(){
         return(
-            <Card>
+            <Card style={{flex:1}}>
                 <CardSection />
+                    <Input
+                        placeholder="user@gmail.com"
+                        label={'Email'}
+                        value={this.state.email}
+                        onChangeText={email => this.setState({email})}
+                    />
                 <CardSection />
-
+                    <Input
+                        secureTextEntry={true}
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({password})}
+                    />
                 <CardSection >
                     <Button>
                         Log In
